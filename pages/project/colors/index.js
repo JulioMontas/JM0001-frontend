10acc91085b32ce8138ef039548b6fb5ccfbb0a8
JulioMontas <hello@juliomontas.com>
2020-12-29 00:12:24 -0500

    #SEO improve page load

--- pages/project/colors/index.js
+++ pages/project/colors/index.js
@@ -10,7 +10,7 @@ import styles from '../../../styles/CaseStudy.module.css'
 function ProjectIndex() {
   return <Layout>
       <Head>
-          <title>Basic Color App Fundamentals - Simple Fun for Toddlers</title>          
+          <title>Basic Color Fundamentals - Simple Fun App for Toddlers</title>          
 
           <meta name='twitter:url' content='https://llegojulio.com/project/colors' />
           <meta property='og:url' content='https://llegojulio.com/project/colors' />
@@ -18,12 +18,12 @@ function ProjectIndex() {
           <meta property='og:image' content='https://llegojulio.com/img/project/colors/slide_03.png' />
           <meta name='twitter:image' content='https://llegojulio.com/img/project/colors/slide_03.png' />
 
-          <meta property='og:title' content='Julio Montas | Basic Color App Fundamentals - Simple Fun for Toddlers' />
-          <meta name='twitter:title' content='Julio Montas | Basic Color App Fundamentals - Simple Fun for Toddlers' />
+          <meta property='og:title' content='Llegó Julio | Basic Color Fundamentals - Simple Fun App for Toddlers' />
+          <meta name='twitter:title' content='Llegó Julio | Basic Color Fundamentals - Simple Fun App for Toddlers' />
 
-          <meta name="description" content="App | Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory" key="description"/>
-          <meta property='og:description' content='App | Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory' />
-          <meta name='twitter:description' content='App | Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory' />
+          <meta name="description" content="Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory" key="description"/>
+          <meta property='og:description' content='Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory' />
+          <meta name='twitter:description' content='Figure out a device for toddler to learn the basic of colors pronunciation and visual recognition memory' />
 
           <link rel="canonical" href="https://llegojulio.com/project/colors" />
       </Head>
@@ -52,10 +52,12 @@ function ProjectIndex() {
                     <h2>&#127968; Mobile App Concept designs</h2>
                     <Image
                     src="/img/project/colors/slide_03.png"
-                    alt="Mockup of python page"
+                    alt="Choose a color to learn"
                     width={1080}
                     height={1080}
+                    quality={20}
                     layout="responsive"
+                    loading="eager"
                     />
                     <Swiper
                     speed={400}
@@ -77,19 +79,23 @@ function ProjectIndex() {
                         <SwiperSlide>
                             <Image
                             src="/img/project/colors/slide_04.png"
-                            alt="Mockup of python page"
+                            alt="Mockup design of the green color"
                             width={1080}
                             height={1080}
+                            quality={20}
                             layout="responsive"
+                            loading="lazy"
                             />
                         </SwiperSlide>
                         <SwiperSlide>
                             <Image
                             src="/img/project/colors/slide_05.png"
-                            alt="Mockup of python page"
+                            alt="Mockup design of the blue color"
                             width={1080}
                             height={1080}
+                            quality={20}
                             layout="responsive"
+                            loading="lazy"
                             />
                         </SwiperSlide>
                     </Swiper>

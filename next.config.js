6b0971f4d4a32264a64be134b89ca45b6250f4e6
JulioMontas <hello@juliomontas.com>
2021-06-11 09:07:54 -0400

    webpack5 update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@ const nextTranslate = require('next-translate');
 
 module.exports = {
   ...nextTranslate(),
-  // Webpack 5 is enabled by default
-  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-  webpack5: false,
-
-  exportPathMap: async function() {
-    return {
-      "/": { page: "/" },
-      "/menu-me": { page: "/menu-me" }
-    };
-  }
+  future: {
+    webpack5: true,
+  },
 };

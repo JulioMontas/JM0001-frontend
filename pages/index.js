8e5c448361cb2657a4d0bce332a687b9892f2809
JulioMontas <hello@juliomontas.com>
2021-07-22 23:27:54 -0400

    homepage css changes

--- pages/index.js
+++ pages/index.js
@@ -56,41 +56,18 @@ export default function SideProject({ data }) {
     animate={{ x: 0, opacity: 1 }}
     exit={{ x: -300, opacity: 0 }}>
     <div className={styles.main}>
-      <Swiper
-      speed={400}
-      // pagination={{ clickable: true }}
-      // onSlideChange={() => console.log('slide change')}
-      // onSwiper={(swiper) => console.log(swiper)}
-      breakpoints={{
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          width: 310
-        },
-        1200: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-          width: 900
-        }
-      }}
-    >
-      <SwiperSlide>
-        <div className={styles.sectionUno}>
-            <Image
-              src="/me.png"
-              alt="self portrait of me wearing sunglasses indoors"
-              width={200}
-              height={200}
-            />
-            <h2>{title}</h2>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className={styles.sectionDos}>
-        <p>{description} <a href="https://julio62.typeform.com/to/IBnyOp">Let's chat</a>.</p>
-      </div>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <Image
+        src="/me.png"
+        alt="self portrait of me wearing sunglasses indoors"
+        width={200}
+        height={200}
+      />
+      <h2>{title}</h2>
+    </section>
+    <section>
+      <p>{description} <a href="https://julio62.typeform.com/to/IBnyOp">Let's chat</a>.</p>
+    </section>
     </div>
     </motion.div>
     </div>

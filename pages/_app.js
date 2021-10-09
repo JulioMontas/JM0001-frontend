d750ee91f709dc8653078e318704332920f1d952
JulioMontas <hello@juliomontas.com>
2021-10-08 21:36:28 -0400

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,39 @@ import { motion, AnimatePresence } from "framer-motion"
 import Head from 'next/head'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Script from 'next/script'
 import '../styles/globals.css'
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
 
-// install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function MyApp({ Component, pageProps }) {
-  return <AnimatePresence exitBeforeEnter>
-    <Head>
-      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' key="viewport" />
-    </Head>
-    <Component {...pageProps} />
-  </AnimatePresence>
+  return (
+    <>
+    <AnimatePresence exitBeforeEnter>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' key="viewport" />
+      </Head>
+       <Script
+         strategy="lazyOnload"
+         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </AnimatePresence>
+    </>
+  )
 }
 
 export default MyApp

b9cbd668b3923ef5374321442b51dd2c27cbd42e
JulioMontas <hello@juliomontas.com>
2022-03-04 21:54:19 -0500

    Head tag add, oops

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from '../styles/Homepage.module.css'
 import useTranslation from 'next-translate/useTranslation'
 import NavBar from '../components/NavBar'
@@ -8,9 +9,34 @@ import SideProjectWrap from '../components/SideProjectWrap'
 import TechStack from '../components/TechStack'
 import Container from '../components/Container'
 
-export default function Homepage() {
+const MY_HOMEPAGE = `
+{
+  homePage(locale: en, fallbackLocales: en) {
+    title
+    content {
+      value
+    }
+  }
+}`;
+
+export default function Homepage({ data }) {
   return (
     <>
+    <Head>
+      <title>Julio Montás - UI Developer • Interaction Designer • Full Stack Designer</title>
+      <meta name='twitter:url' content='https://juliomontas.com/blog/' />
+      <meta property='og:url' content='https://juliomontas.com/blog/' />
+      <meta property='og:image' content='https://juliomontas.com/me.png' />
+      <meta name='twitter:image' content='https://juliomontas.com/me.png' />
+      <meta property='og:title' content='Blog - UI Developer • Interaction Designer • Full Stack Designer | Julio Montás' />
+      <meta name='twitter:title' content='Blog - UI Developer • Interaction Designer • Full Stack Designer | Julio Montás' />
+      <meta name="description" content="Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC." key="description"/>
+      <meta property='og:description' content='Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC.' />
+      <meta name='twitter:description' content='Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC.' />
+      <link rel="canonical" href="https://juliomontas.com/" />
+      <link rel="alternate" href="https://juliomontas.com/" hreflang="en-us" />
+      <link rel="alternate" href="https://juliomontas.com/es-us/" hreflang="es-us" />
+    </Head>
       <div className={styles.main}>
       <Container>
         <HeroSection

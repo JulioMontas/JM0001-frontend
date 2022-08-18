2e4206a1e598105f0460965a8d6d1941145ae95b
JulioMontas <hello@juliomontas.com>
2022-08-18 13:50:11 -0400

    new page

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,36 +1,59 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { motion } from "framer-motion"
 import stylesHome from '../styles/Homepage.module.css'
 import useTranslation from 'next-translate/useTranslation'
 import NavBar from '../components/NavBar'
 import LinkList from '../components/LinkList'
 import Container from '../components/Container'
 
+const easing = [.6, -.05, .01, .99];
+
+const fadeInUp = {
+  initial:{
+    y: 60,
+    opacity: 0
+  },
+  animate:{
+    y: 0,
+    opacity: 1,
+    transition:{
+      duration: .6,
+      ease: easing
+    }
+  }
+};
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
 export default function MePage({ data }) {
   return (
     <>
-    <Head>
-      <title>Julio Montas - UI Developer • Full Stack Designer</title>
-      <meta name='twitter:url' content='https://juliomontas.com/me' />
-      <meta property='og:url' content='https://juliomontas.com/me' />
+      <Head>
+      <title>Julio Montas - About Me</title>
+      <meta name='twitter:url' content='https://juliomontas.com/about-me' />
+      <meta property='og:url' content='https://juliomontas.com/about-me' />
       <meta property='og:image' content='https://juliomontas.com/me.png' />
       <meta name='twitter:image' content='https://juliomontas.com/me.png' />
-      <meta property='og:title' content='Julio montas - UI Developer • Full Stack Designer' />
-      <meta name='twitter d:title' content='Julio montas - UI Developer • Full Stack Designer' />
+      <meta property='og:title' content='Julio montas - About Me' />
+      <meta name='twitter d:title' content='Julio montas - About Me' />
       <meta name="description" content="Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC." key="description"/>
       <meta property='og:description' content='Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC.' />
       <meta name='twitter:description' content='Building Custom Website, eCommerce, CMS and Mobile App Prototype. Experience with Startup, Private Companies and Creative Agency. NYC.' />
-      <link rel="canonical" href="https://juliomontas.com/me" />
-      <link rel="alternate" href="https://juliomontas.com/me" hreflang="en-us" />
-      <link rel="alternate" href="https://juliomontas.com/es-us/me" hreflang="es-us" />
-    </Head>
-      <div className={stylesHome.main}>
-        <NavBar />
+      <link rel="canonical" href="https://juliomontas.com/about-me" />
+      <link rel="alternate" href="https://juliomontas.com/about-me" hreflang="en-us" />
+      <link rel="alternate" href="https://juliomontas.com/es-us/about-me" hreflang="es-us" />
+      </Head>
+      <NavBar />
+      <div className="backgroundColor">
         <Container>
-
-        <h1>Hola!</h1>
-
-         </Container>
+        </Container>
       </div>
     </>
   );
